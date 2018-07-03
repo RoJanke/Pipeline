b1016364ecdcc2133f718e30458ee37a460522e3
RoJanke <robin.janke@brockhaus-ag.de>
2018-07-03 14:23:04 +0200

    Test added

---
+++ src/main/java/com/example/demo/MyMath.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class MyMath {
+    public double add(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public double subtract(double number1, double number2) {
+        return number1 - number2;
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
+        fail("Test Failed");
     }
 
 }
